bdecaba9b6712337013a85423ca01d4be9d4a71a
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-03 11:33:09 +0000

    react vs vanilla js

--- react_info_site/index.js
+++ react_info_site/index.js
@@ -1,7 +1,11 @@
-ReactDOM.render(
-  <ul>
-    <li>grocery store</li>
-    <li>Check the schedule</li>
-  </ul>,
-  document.getElementById("root")
-);
+//Imperative way of creating some element
+/* const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1); */
+
+function Contents() {
+  return <h1>this is body 1</h1>;
+}
+
+ReactDOM.render(<h1>hello</h1>, document.getElementById("root"));
