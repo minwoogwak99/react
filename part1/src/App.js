3bd42d027ed476cd3295caac940bf1c95bbdd628
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-05 12:02:56 +0000

    starting learning useState

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,11 @@
-const App = () => (
-  <div>
-    <h1>Hello world</h1>
-  </div>
-);
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  return <div>{counter}</div>;
+};
+
 export default App;
