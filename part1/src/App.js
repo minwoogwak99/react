337a32dc1efbc697184dde1f449a74cc1197751f
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-06 10:05:34 +0000

    Button Event Handler

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,9 +3,17 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  const handleClick = () => {
+    console.log("button clicked");
+    setCounter(counter + 1);
+  };
 
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>add 1</button>
+    </div>
+  );
 };
 
 export default App;
