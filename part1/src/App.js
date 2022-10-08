9cd32d0712602ed86c5d1f00ead07f6941dff405
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-08 05:42:03 +0000

    more complex state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 
 const ComplexState = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState({ left: 0, right: 0 });
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {clicks.left}
+      <button
+        onClick={() =>
+          setClicks({ left: clicks.left + 1, right: clicks.right })
+        }
+      >
+        left
+      </button>
+      <button
+        onClick={() =>
+          setClicks({ left: clicks.left, right: clicks.right + 1 })
+        }
+      >
+        right
+      </button>
+      {clicks.right}
     </div>
   );
 };
