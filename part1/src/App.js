baa05e438718a587cc11c3010fde541e7a2bfe10
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-08 05:58:00 +0000

    dealing with array

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 const ComplexState = () => {
-  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  /* const handleLeftClick = () => {
     const newClick = {
       ...clicks,
       left: clicks.left + 1,
@@ -16,17 +28,19 @@ const ComplexState = () => {
     const newClick = {
       ...clicks,
       right: clicks.right + 1,
-    };
+    }; 
 
     return newClick;
   };
+  */
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={() => setClicks(handleLeftClick)}>left</button>
-      <button onClick={() => setClicks(handleRightClick)}>right</button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <div>{allClicks}</div>
     </div>
   );
 };
