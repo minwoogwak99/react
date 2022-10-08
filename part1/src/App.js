3a61ada1d581dd20d40585349cd483b016e4431e
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-08 04:25:23 +0000

    added a second buttons to play with

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,45 @@
 import { useState } from "react";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
+const ComplexState = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
+    </div>
+  );
 };
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  const decreaseByOne = () => setCounter(counter - 1);
   const inscreaseByOne = () => setCounter(counter + 1);
-
   const reset = () => setCounter(0);
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={inscreaseByOne} text="add 1" />
-      <Button onClick={reset} text="reset to 0" />
+      <div>
+        <Display counter={counter} />
+        <Button onClick={inscreaseByOne} text="add 1" />
+        <Button onClick={decreaseByOne} text="remove 1" />
+        <Button onClick={reset} text="reset to 0" />
+      </div>
+      <div>
+        <ComplexState />
+      </div>
     </div>
   );
 };
