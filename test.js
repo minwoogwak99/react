15262b96616665965fa6d369fbd87da965a82db7
Minwoo Gwak <mgwak2@illinois.edu>
2022-10-10 05:48:13 +0000

    part2.a rendering collection

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-var animals = [
+let animals = [
   { name: "Fluffykins", species: "rabbit" },
   { name: "Caro", species: "dog" },
   { name: "Hamilton", species: "dog" },
   { name: "Harold", species: "fish" },
   { name: "Ursula", species: "cat" },
   { name: "Jimmy", species: "fish" },
+];
+
+let orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 },
+  { amount: 120 },
 ];
 
 function isDog(animals) {
@@ -23,3 +31,17 @@ console.log("Using a regular for loop to get a names:", arr);
 /* Same function using a map function */
 let arr2 = animals.map((animal) => animal.name);
 console.log("Using a map function to get a names: ", arr2);
+
+/* get totalAmount using regular for loop */
+let totalAmount = 0;
+for (let i = 0; i < orders.length; i++) {
+  totalAmount += orders[i].amount;
+}
+console.log("totalAmount using a regular for loop:", totalAmount);
+
+/* Same functionality using a reduce function */
+let totalAmount2 = orders.reduce((start, lop) => {
+  console.log(start, lop);
+  return start + lop.amount;
+}, 0);
+console.log("smae functionality using a reduce: ", totalAmount2);
